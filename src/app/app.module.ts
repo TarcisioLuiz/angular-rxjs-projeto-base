c0b5cebd484471a29419f40c6e65286dea75ec38
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2023-07-11 23:57:28 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { LivroComponent } from './componentes/livro/livro.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { ListaLivrosComponent } from './views/lista-livros/lista-livros.component';
+import { ModalLivroComponent } from './views/modal-livro/modal-livro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    LivroComponent,
+    ListaLivrosComponent,
+    ModalLivroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
