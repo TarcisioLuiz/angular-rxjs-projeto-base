5aeb1e1dddbe54b769bb40c9f4fc63179a8d0849
Tarcisiosilvahive <tarcisio.silva@hivecloud.br>
2023-07-17 18:00:58 -0300

    tratamento de erros

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -3,7 +3,10 @@ import { LivroService } from './../../service/livro.service';
 import { Component } from '@angular/core';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { FormControl } from '@angular/forms';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { catchError, debounceTime, filter, map, switchMap, tap } from 'rxjs/operators';
+import { EMPTY, throwError } from 'rxjs';
+
+const PAUSA = 300
 
 @Component({
   selector: 'app-lista-livros',
@@ -12,14 +15,23 @@ import { map, switchMap, tap } from 'rxjs/operators';
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl()
+  mensagemErro = ''
 
   constructor(private service: LivroService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
     tap(() => console.log('Fluxo inicial')),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    tap(() => console.log('Requisição ao servidor')),
-    map((items) => this.livrosResultadoParaLivros(items))
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError(erro => {
+      this.mensagemErro = 'Ops ocorreu um erro, Recarregue a aplicação'
+      return EMPTY
+      //console.log(erro)
+      //return throwError(() => new Error(this.mensagemErro = 'Ops ocorreu um erro, Recarregue a aplicação'))
+  })
     )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
