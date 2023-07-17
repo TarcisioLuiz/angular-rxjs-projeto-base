1de535ba43fb061a8584445cc378dd15ae99202d
Tarcisiosilvahive <tarcisio.silva@hivecloud.br>
2023-07-17 18:25:44 -0300

    implementação da contagem de resultados

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,4 +1,4 @@
-import { Item, LivrosResultado } from './../models/interfaces';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -13,12 +13,12 @@ export class LivroService {
 
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado)
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+    return this.http.get<LivrosResultado>(this.API, { params })
+    //.pipe(
       //tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
-      map(resultado => resultado.items),
-      //tap(resultado => console.log('Fluxo após o map', resultado))
-    )
+     // map(resultado => resultado.items ?? []),
+      //tap(resultado => console.log('Fluxo após o map', resultado)))
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,10 +1,10 @@
-import { Item } from './../../models/interfaces';
+import { Item, LivrosResultado } from './../../models/interfaces';
 import { LivroService } from './../../service/livro.service';
 import { Component } from '@angular/core';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { FormControl } from '@angular/forms';
 import { catchError, debounceTime, filter, map, switchMap, tap } from 'rxjs/operators';
-import { EMPTY, throwError } from 'rxjs';
+import { EMPTY, of, throwError } from 'rxjs';
 
 const PAUSA = 300
 
@@ -16,8 +16,21 @@ const PAUSA = 300
 export class ListaLivrosComponent {
   campoBusca = new FormControl()
   mensagemErro = ''
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro)
+      return of()
+    })
+  )
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
@@ -25,12 +38,13 @@ export class ListaLivrosComponent {
     tap(() => console.log('Fluxo inicial')),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
     tap((retornoAPI) => console.log(retornoAPI)),
+    map(resultado => resultado.items ?? []),
     map((items) => this.livrosResultadoParaLivros(items)),
     catchError(erro => {
-      this.mensagemErro = 'Ops ocorreu um erro, Recarregue a aplicação'
-      return EMPTY
-      //console.log(erro)
-      //return throwError(() => new Error(this.mensagemErro = 'Ops ocorreu um erro, Recarregue a aplicação'))
+      //this.mensagemErro = 'Ops ocorreu um erro, Recarregue a aplicação'
+      //return EMPTY
+      console.log(erro)
+      return throwError(() => new Error(this.mensagemErro = 'Ops ocorreu um erro, Recarregue a aplicação'))
   })
     )
 
